d372139b7c97452948abb5570964c48ed2e33fad
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-06-19 13:23:47 +0700

    chore: add `cloudinary` as a main source

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: false,
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  env: ".env",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
